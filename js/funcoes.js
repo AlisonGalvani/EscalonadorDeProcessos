91ec856998c40f84cfa62b5703aa2ea6aba735fc
Alison Galvani <alisonfgalvani@gmail.com>
2018-07-01 23:41:34 -0300

    Versão 1 completa

--- js/funcoes.js
+++ js/funcoes.js
@@ -19,24 +19,21 @@ function Processos(_nome, _prioridade, _tempo){
 function inserirProcesso() {
 
     //capturando inputs
-    var nome = document.getElementById('inputNome').value;
+    var nome       = document.getElementById('inputNome').value;
     var prioridade = parseInt(document.getElementById('inputPrioridade').value);
-    var tempo = parseInt(document.getElementById('inputTempo').value);
+    var tempo      = parseInt(document.getElementById('inputTempo').value);
 
     //zerando inputs visual
-    document.getElementById('inputNome').value = "";
+    document.getElementById('inputNome').value       = "";
     document.getElementById('inputPrioridade').value = "";
-    document.getElementById('inputTempo').value = "";
+    document.getElementById('inputTempo').value      = "";
 
     novoProcesso = new Processos(nome, prioridade, tempo);                       //criando novo objeto processo
     Processo.push(novoProcesso);                                                 //atribuindo ao array de processos o novo processo
  
     tempoTotalProcessos += novoProcesso.tempo;                                   //atribuindo o tempo do processo a variavel contadora 
-    tempoTotalExecucao += tempoTotalProcessos;                                   //atribuindo o tempo de execucao variavel contadora
-    console.log("Tempo total: ", tempoTotalExecucao)                           	        
+    tempoTotalExecucao += tempoTotalProcessos;                                   //atribuindo o tempo de execucao variavel contadora                        	        
     mediaTempoExecucao = (tempoTotalExecucao/Processo.length).toFixed(1);        //calculando tempo médio da execucao e formatando com uma casa decimal (toFixed(1))
-    console.log("Media: ", mediaTempoExecucao)
-    console.log("T: ", Processo.length)
     atualizarGrafico(graficoChart,[novoProcesso.nome], [tempoTotalProcessos]);   //atualizando gráfico com o novo processo
     addProcessoVisual(novoProcesso, tempoTotalProcessos);                        //inserindo visualmente o processo no rodapé da página   
     
@@ -45,25 +42,24 @@ function inserirProcesso() {
 // função que escalona os processos, recebendo como parametro o tipo do escalonamento
 function escalonar(modo) {
 
-    tempoTotalProcessos = 0;     //zerando variavel que armazena tempo total
-    tempoTotalExecucao=0;
-    mediaTempoExecucao=0;
-    var ProcessoAuxiliar = new Array(); //criando array de processo auxiliar que recebera os processos ordenados
+    tempoTotalProcessos  = 0;                //zerando variavel que armazena tempo total
+    tempoTotalExecucao   =0;
+    mediaTempoExecucao   =0;
+    var ProcessoAuxiliar = new Array();     //criando array de processo auxiliar que recebera os processos ordenados
+    
 
-    graficoChart.destroy();     //destroindo o gráfico para criar um novo ordenado                                                                 
-    Criagrafico();              //criando um novo gráfico ainda vazio
-    limparProcessoVisual();     //limpando os processos do rodapé
+    graficoChart.destroy();                 //destroindo o gráfico para criar um novo ordenado                                                                 
+    Criagrafico();                          //criando um novo gráfico ainda vazio
+    limparProcessoVisual();                 //limpando os processos do rodapé
 
     switch (modo) {
 
         //se fifo
-        case "fifo":
-            console.log("Processo: ", Processo);
-            // ProcessoAuxiliar = selectionSortFifo(Processo); 
+        case "fifo": 
             for (row in Processo) {                                                         //percorrendo os processos
                 tempoTotalProcessos = tempoTotalProcessos + Processo[row].tempo;            //somando tempo total dos processos
-                tempoTotalExecucao += tempoTotalProcessos;
-                mediaTempoExecucao = (tempoTotalExecucao/Processo.length).toFixed(1);
+                tempoTotalExecucao  += tempoTotalProcessos;                                 //calculando tempo total de execução
+                mediaTempoExecucao  = (tempoTotalExecucao/Processo.length).toFixed(1);      //calculando a media de tempo de execução dos processos, formatando com uma casa decimal
                 addProcessoVisual(Processo[row], tempoTotalProcessos);                      //adicionando processo no rodapé
                 atualizarGrafico(graficoChart, Processo[row].nome, tempoTotalProcessos);    //adicionando processo ao gráfico
             }            
@@ -75,75 +71,80 @@ function escalonar(modo) {
 
             for (row in ProcessoAuxiliar) {                                                         //percorrendo os processos
                 tempoTotalProcessos = tempoTotalProcessos + ProcessoAuxiliar[row].tempo;            //somando tempo total dos processos
-                tempoTotalExecucao += tempoTotalProcessos;
-                mediaTempoExecucao = (tempoTotalExecucao/Processo.length).toFixed(1);
-                addProcessoVisual(Processo[row], tempoTotalProcessos);                                       //adicionando processo no rodapé
+                tempoTotalExecucao  += tempoTotalProcessos;                                         //calculando tempo total de execução
+                mediaTempoExecucao  = (tempoTotalExecucao/Processo.length).toFixed(1);              //calculando a media de tempo de execução dos processos, formatando com uma casa decimal
+                addProcessoVisual(Processo[row], tempoTotalProcessos);                              //adicionando processo no rodapé
                 atualizarGrafico(graficoChart, ProcessoAuxiliar[row].nome, tempoTotalProcessos);    //adicionando processo ao gráfico
             }
         break;
 
         //se prioridade    
         case "prioridade":
-            ProcessoAuxiliar = selectionSortPrioridade(Processo);                                  //Chamando função de ordenação por tempo, passando o array de processos e atribuindo o retorno para a var auxiliar
-            
+            ProcessoAuxiliar = selectionSortPrioridade(Processo);                                  //Chamando função de ordenação por tempo, passando o array de processos e atribuindo o retorno para a var auxiliar            
 
             for (row in ProcessoAuxiliar) {                                                        //percorrendo os processos
                 tempoTotalProcessos = tempoTotalProcessos + ProcessoAuxiliar[row].tempo;           //somando tempo total dos processos
-                tempoTotalExecucao += tempoTotalProcessos;
-                mediaTempoExecucao = (tempoTotalExecucao/Processo.length).toFixed(1);
-                addProcessoVisual(ProcessoAuxiliar[row], tempoTotalProcessos);                              //adicionando processo no rodapé
+                tempoTotalExecucao  += tempoTotalProcessos;                                        //calculando tempo total de execução
+                mediaTempoExecucao  = (tempoTotalExecucao/Processo.length).toFixed(1);             //calculando a media de tempo de execução dos processos, formatando com uma casa decimal
+                addProcessoVisual(ProcessoAuxiliar[row], tempoTotalProcessos);                     //adicionando processo no rodapé
                 atualizarGrafico(graficoChart, ProcessoAuxiliar[row].nome, tempoTotalProcessos);   //adicionando processo ao gráfico
             }
         break;
 
         case "circular":
-            var tempoCircular=0;
-            console.log("OPAA2222: ", Processo);
-            ProcessoAuxiliar = Processo;
-            ProcessoOrdenadoFifo = new Array();
+            ProcessoAuxiliar = JSON.stringify(Processo)                                            //copiando processos para processo auxiliar em forma de string
+            ProcessoAuxiliar = JSON.parse(ProcessoAuxiliar)                                        //transformando a string em onjeto novamente
+            ProcessoOrdenadoCircular = new Array();                                                //Array que irá guardar os processos ordenados por circular
+            var tempoCircular=0;                                                                   //Variavel que soma os tempos do circular
+            var indice=0;                                                                          //indice para controlar a reorganização dos tempos
+            var flag = false;                                                                      //flag usada para saber quando excluir um processo do array de processo auxiliar                           
+            var indiceExclusao;                                                                    //variavel que armazena o indice de qual posicao de auxiliar dev ser excluida
+            var tempoTotal = new Array();                                                          //Array que armazena o tempo total de cada termino de processo
             
-            console.log("Tamanho fora if: ", ProcessoAuxiliar.length, ProcessoAuxiliar);
-       var j=0
-                while(ProcessoAuxiliar.length > 0){ //enquanto existir processos
-                    j++
-                    for (row in ProcessoAuxiliar){
-                        ProcessoAuxiliar[row].tempo--;
-                        console.log(`Processo: ${ProcessoAuxiliar[row].nome}, Tempo: ${ProcessoAuxiliar[row].tempo}`);
-                        tempoCircular++;
-                        if(ProcessoAuxiliar[row].tempo == 0){
-                            ProcessoOrdenadoFifo.push(Processo[row]);
-                            console.log("OPAA: ", Processo[row]);
-                            ProcessoOrdenadoFifo.tempoDeExecucao = tempoCircular;
-                            ProcessoAuxiliar.splice(ProcessoAuxiliar[row-1,1]);
-                            // for (i=ProcessoAuxiliar[row]; i<ProcessoAuxiliar.length-1; i++){
-                            //     ProcessoAuxiliar[i] = ProcessoAuxiliar[i+1];
-                            // }
-                            // ProcessoAuxiliar.pop();
-                            console.log("Tamanho dentro do if: ", ProcessoAuxiliar.length, ProcessoAuxiliar)
-                        }
+
+            //enquanto existir processos
+            while(ProcessoAuxiliar.length > 0){ 
+
+                //percorre os processos
+                for (row in ProcessoAuxiliar)
+                {
+                    ProcessoAuxiliar[row].tempo--;                                                              //diminui o tempo dos processos
+                    tempoCircular++;                                                                            //aumenta em 1 tempos o tempo total de execução    
+                    mediaTempoExecucao = (tempoCircular/Processo.length).toFixed(1);                            //calcula a média do tempo de execução e formata com uma casa decimal
+
+                    //se o tempo do processo lido chegou a 0
+                    if(ProcessoAuxiliar[row].tempo === 0){                       
+                        ProcessoOrdenadoCircular.push(ProcessoAuxiliar[row]);                                   //joga o processo pra variavel que contem os processos ordenados por circular
+                        tempoTotal.push(tempoCircular);                                                         //inserindo o tempo de término desse processo no array de tempos
+                        var flag = true;
+                        indiceExclusao = row;                                                                   //guardando posicao do processo que deve ser excluido do array auxiliar
+                    }
+                    
+                }
+                if (flag){
+                    ProcessoAuxiliar.splice(indiceExclusao,1);                                                  //Excluindo o processo que terminou de executa do array auxliar
+                    flag = false;                                          
                 }
             }
-            console.log("ordenado: ", ProcessoOrdenadoFifo);
+
+            //INSERINDO NOVAMENTE O TEMPO DE CADA PROCESSO GUARDADO NO ARRAY DE TEMPOS, DEVIDO AO TEMPO DE CADA PROCESSO TER SIDO ZERADO NO CIRCULAR 
+            indice=0;
+            while(indice <= Processo.length-1){
+                for(row in Processo){
+                    if(ProcessoOrdenadoCircular[indice].indice === Processo[row].indice){
+                        ProcessoOrdenadoCircular[indice].tempo = Processo[row].tempo;
+                        break;                        
+                    } 
+                }
+                addProcessoVisual(ProcessoOrdenadoCircular[indice], tempoTotal[indice]);                      //adicionando processo no rodapé
+                atualizarGrafico(graficoChart, ProcessoOrdenadoCircular[indice].nome, tempoTotal[indice]);    //adicionando processo ao gráfico
+                indice++;
+            }
         break;
     }
 
 }
 
-//Selection sorte para ordenação por Fifo
-function selectionSortFifo(array) {
-    for (var i = 0; i < array.length; i++) {
-        var min = i;
-        for (var j = i + 1; j < array.length; j++) {
-            if (array[j].indice < array[min].indice) {
-                min = j;
-            }
-        }
-        var temp = array[i];
-        array[i] = array[min];
-        array[min] = temp;
-    }
-    return array;
-};
 
 //Selection sorte para ordenação por tempo
 function selectionSortTempo(array) {
